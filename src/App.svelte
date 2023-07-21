c0935e024dbc67882c772ac0df161040f4eb6fd5
Stella-Bgll <stella.buguellou@oclock.school>
2023-07-21 14:18:06 +0200

    Minuteurs

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Timer from "./lib/Timer.svelte";
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<div>
+  <Timer />
+</div>
+<div>
+  <Timer />
+</div>
+<div>
+  <Timer />
+</div>
+
+<style>
+  div {
+    display: flex;
+    justify-content: center;
+    border: solid  rgb(12, 201, 235) 4px;
+    border-radius: 50%;
+    margin: 60px 250px;
+    padding: 60px 60px;
+  }
+</style>
---
+++ src/lib/Timer.svelte
@@ -0,0 +1,24 @@
+<script>
+  let seconds = 0;
+  const incrementSeconds = () => {
+    seconds++;
+  };
+  setInterval(incrementSeconds, 1000);
+
+  const reset = () => {
+    seconds = 0;
+  };
+</script>
+
+<div>
+  <label for="">Secondes écoulées</label>
+  <p>{seconds}</p>
+  <button on:click={reset}>Remise à zéro</button>
+</div>
+
+<style>
+  button {
+   color: blue;
+   background: rgb(211, 13, 145);
+ }
+</style>
